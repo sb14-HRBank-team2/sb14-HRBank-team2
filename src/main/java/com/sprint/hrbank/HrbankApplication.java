c1de4f28118b957e72bbf15a0cb3da10aaea3e1a
parksunovo <sun930325@gmail.com>
2026-09-08 15:25:26 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/sprint/hrbank/HrbankApplication.java
@@ -0,0 +1,12 @@
+package com.sprint.hrbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrbankApplication.class, args);
+    }
+}
---
+++ src/test/java/com/sprint/hrbank/HrbankApplicationTests.java
@@ -0,0 +1,11 @@
+package com.sprint.hrbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrbankApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
