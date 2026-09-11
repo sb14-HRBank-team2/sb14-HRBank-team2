454ac63cd34cd55381e338f1eb3c9f196ba5c630
rladjwls02 <safhao7152@gmail.com>
2026-09-11 09:27:52 +0900

    style: Diff 엔티티 포맷 적용

--- src/main/java/com/sprint/hrbank/entity/Diff.java
+++ src/main/java/com/sprint/hrbank/entity/Diff.java
@@ -11,31 +11,30 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Diff {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Integer id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Integer id;
 
-    @Column(nullable = false)
-    String propertyName;
+  @Column(nullable = false)
+  String propertyName;
 
-    String before;
+  String before;
 
-    @Column(nullable = false)
-    String after;
+  @Column(nullable = false)
+  String after;
 
-    @ManyToOne
-    @JoinColumn(name = "change_log_id", nullable = false)
-    ChangeLog changeLog;
+  @ManyToOne
+  @JoinColumn(name = "change_log_id", nullable = false)
+  ChangeLog changeLog;
 
-    private Diff(String propertyName, String before, String after, ChangeLog changeLog) {
-        this.propertyName = propertyName;
-        this.before = before;
-        this.after = after;
-        this.changeLog = changeLog;
-    }
+  private Diff(String propertyName, String before, String after, ChangeLog changeLog) {
+    this.propertyName = propertyName;
+    this.before = before;
+    this.after = after;
+    this.changeLog = changeLog;
+  }
 
-    public static Diff create(
-            String propertyName, String before, String after, ChangeLog changeLog) {
-        return new Diff(propertyName, before, after, changeLog);
-    }
+  public static Diff create(String propertyName, String before, String after, ChangeLog changeLog) {
+    return new Diff(propertyName, before, after, changeLog);
+  }
 }
