d89d3651dd8b3111a8d203b38cfa8f0f4cc4affb
parksunovo <sun930325@gmail.com>
2026-09-12 21:51:57 +0900

    feat: 직원 컨트롤러 내 목록요청 메서드 구현

--- src/main/java/com/sprint/hrbank/adapter/webapi/employee/EmployeeController.java
+++ src/main/java/com/sprint/hrbank/adapter/webapi/employee/EmployeeController.java
@@ -1,13 +1,16 @@
 package com.sprint.hrbank.adapter.webapi.employee;
 
+import com.sprint.hrbank.adapter.persistence.employee.EmployeeSearchCond;
 import com.sprint.hrbank.application.employee.EmployeeCommandService;
 import com.sprint.hrbank.application.employee.EmployeeQueryService;
+import com.sprint.hrbank.application.employee.dto.CursorPageResponseEmployeeDto;
 import com.sprint.hrbank.application.employee.dto.EmployeeDto;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -25,6 +28,14 @@ public class EmployeeController {
   @ResponseStatus(HttpStatus.OK)
   public EmployeeDto getEmployeeDetail(@PathVariable Integer id) {
     return employeeService.getById(id);
+  }
+
+  @GetMapping
+  @ResponseStatus(HttpStatus.OK)
+  public CursorPageResponseEmployeeDto getEmployeePage(
+      @ModelAttribute EmployeeSearchCond cond
+  ) {
+    return employeeService.getEmployeePage(cond);
   }
 
   @DeleteMapping("/{id}")
