dece7d83fbacd2cd4a5052c76818c2c79be2cf3d
rladjwls02 <safhao7152@gmail.com>
2026-09-13 19:10:33 +0900

    refactor: Query, Command service분리

--- src/main/java/com/sprint/hrbank/application/changelog/ChangeLogCommandService.java
+++ src/main/java/com/sprint/hrbank/application/changelog/ChangeLogCommandService.java
@@ -1,23 +1,19 @@
 package com.sprint.hrbank.application.changelog;
 
-import com.sprint.hrbank.adapter.persistence.changelog.ChangeLogSearchCond;
 import com.sprint.hrbank.application.changelog.dto.ChangeLogCreateRequestDto;
 import com.sprint.hrbank.application.changelog.dto.ChangeLogResponseDto;
 import com.sprint.hrbank.application.changelog.dto.DiffCreateRequestDto;
 import com.sprint.hrbank.application.changelog.required.ChangeLogRepository;
-import com.sprint.hrbank.application.changelog.required.DiffRepository;
 import com.sprint.hrbank.domain.chagelog.ChangeLog;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class ChangeLogService {
+public class ChangeLogCommandService {
 
   private final ChangeLogRepository changeLogRepository;
-  private final DiffRepository diffRepository;
   private final DiffService diffService;
 
   @Transactional
@@ -28,13 +24,7 @@ public class ChangeLogService {
     // 변경내역 저장로직
     for (DiffCreateRequestDto target : dto.diffs()) {
       diffService.create(target, result.getId()); // db 2번접근
-      // diffRepository.save(target.toEntity(result)); //db 1번접근
     }
     return ChangeLogResponseDto.from(result);
-  }
-
-  @Transactional(readOnly = true)
-  public List<ChangeLogResponseDto> getChangeLog(ChangeLogSearchCond cond) {
-    return changeLogRepository.search(cond).stream().map(ChangeLogResponseDto::from).toList();
   }
 }
